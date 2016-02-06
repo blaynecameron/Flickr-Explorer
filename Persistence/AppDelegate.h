33c499cbee3ad994c3f8bbd2e268b7e37f12b800
Blayne Chong <blaynechong@gmail.com>
2016-02-06 16:33:59 -0500

    Initial Commit

---
+++ Persistence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Persistence
+//
+//  Created by Blayne Chong on 2016-02-06.
+//  Copyright © 2016 Blayne Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Persistence/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Persistence
+//
+//  Created by Blayne Chong on 2016-02-06.
+//  Copyright © 2016 Blayne Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
